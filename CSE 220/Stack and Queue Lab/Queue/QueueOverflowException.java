2d664a0d9a4d17d09e144361f24ddd98092d552c
Shadab <68025292+Es-Di-Bee@users.noreply.github.com>
2021-12-06 20:44:22 +0600

    Delete CSE 220 directory

--- CSE 220/Stack and Queue Lab/Queue/QueueOverflowException.java
+++
@@ -1,6 +0,0 @@
-public class QueueOverflowException extends Exception {
-    public QueueOverflowException() {
-        super();
-        System.out.println("Queue Full! Cannot Enqueue");
-    }
-}
--- CSE 220/Stack and Queue Lab/Queue/QueueUnderflowException.java
+++
@@ -1,6 +0,0 @@
-public class QueueUnderflowException extends Exception {
-    public QueueUnderflowException() {
-        super();
-        System.out.println("Queue Empty");
-    }
-}
